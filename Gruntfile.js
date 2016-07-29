70c1fb13af37b4d2f7513251d05bf03f0b521929
Andrew Nicols <andrew@nicols.co.uk>
2016-07-29 13:23:06 +0800

    Add Travis configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,6 +53,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-umd');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask("js", ["umd:all", "mocha_istanbul:coverage"]);
+    grunt.registerTask("js", ["umd:all", "test"]);
+    grunt.registerTask("test", ["mocha_istanbul:coverage"]);
     grunt.registerTask("default", ["js", "watch"]);
 };
